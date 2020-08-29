596d61124882ab8b9fdeb14dce8c5c15e08aebb0
Brad Nelson <bnelson13@gmail.com>
2020-08-28 18:02:46 -0700

    Added about me section and typewriter completion

--- js/app.js
+++ js/app.js
@@ -1,17 +1,53 @@
-let typeI = 0;
-let typeText = ['Hello.','My name is Brad','Welcome'];
-let typeWriterDiv = document.querySelector('.typewrite');
+$('#typewrite').typeIt({
+    speed: 125,
+    breakLines: false,
+    lifeLike: true,
+    autoStart: true,
+    loop: false,
+    html: true,
+})
+.tiPause(2000)
+.tiType("Hello. ")
+.tiPause(500)
+.tiDelete(2)
+.tiType('! ')
+.tiPause(2000)
+.tiDelete(7)
+.tiType("I'm Bad, wel")
+.tiDelete(7)
+.tiType('rad. Welcome!')
+.tiPause(2000)
+.tiDelete(18)
+.tiType('Brad Nelson')
 
-function typeWriter() {
-    let i =0;
-    let txt = typeText[i];
-    if (typeI < txt.length) {
-        typeWriterDiv.innerHTML += txt.charAt(typeI);
-        typeI++;
-        setTimeout(typeWriter, 160);
-    }
+setTimeout(function() {$('#typewrite').find('.ti-cursor').addClass('is-hidden')}, 16300);
+setTimeout(function() {document.querySelector('#down-svg').classList = 'welcome-down-svg'}, 16300)
+
+/* Slow scroll to internal links */
+//Puts all a tags with # targets in nodeList
+let anchorLinks = document.querySelectorAll('a[href^="#"]')
+//Iterates through each a tag
+for (let anchorLink of anchorLinks) { 
+    //Applies click listener on each tag and a function
+    anchorLink.addEventListener('click', (e)=> {
+        //Identify each links target
+        let anchorValue = anchorLink.getAttribute('href')
+        let anchorTarget = document.querySelector(anchorValue)
+        //Uses behavior of scroll method to make it slow
+        anchorTarget.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+        //Updates browswer url so it doesn't default to jumping to link
+        history.pushState(null, null, anchorValue)
+        //Prevents the link from functioning as normal
+        e.preventDefault()
+    })
 }
 
-window.onload = setTimeout(typeWriter, 3000);
-
-
+(function titleScroller(text) {
+    document.title = text;
+    setTimeout(function () {
+        titleScroller(text.substr(1) + text.substr(0, 1));
+    }, 100);
+}(" // BRAD NELSON - Front End Web Developer"));
